fbc5368c1486d9efa0a6ae1158f8ce6889a94903
YudithM <yudithmercadoviz@gmail.com>
2023-05-25 14:29:03 -0300

    agregue tarjetas

---
+++ src/componentes/tarjetas.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../estilos/tarjetas.css';
+import 'react-card-flip';
+
+
+function Tarjeta(props) {
+    return(
+           <div className="card__father">
+                <div className="card">
+                    <div className="card__front">
+                     <img className= 'imagen__card'
+                          src= {require (`../imagenes/${props.imagen}`)}
+                          alt={props.altImagen}/>
+                        <div className="body__card_front">
+                            <h1>{props.titulo}</h1>
+                        </div>
+                        </div>
+                    <div className="card__back">
+                        <div className='body__card_back'>
+                            <h2>{props.titulo}</h2>
+                            <p>{props.subtitulo}</p>
+                            <p>{props.resumen}</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+
+);
+}
+
+export default Tarjeta
