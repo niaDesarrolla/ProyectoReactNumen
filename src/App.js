0aa325606dba5c86b108df839d9e807858302bbc
Karelys Castro <129459178+KarelysCastro21@users.noreply.github.com>
2023-05-30 16:17:26 -0300

    actualizacion componente weather api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import './App.css';
 import Cabecera from './componentes/Cabecera';
-import WeatherApp from './componentes/weatherapi';
+import WeatherApp from './componentes/Weatherapi';
 import Tarjeta from './componentes/tarjetas';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <Cabecera />
+        
+          <WeatherApp />
        
-       <Cabecera/>
 
+          <Tarjeta />
        
 
-       <WeatherApp/>
-
-
-       <Tarjeta/>
-       
       </header>
     </div>
   );
--- src/componentes/weatherapi.js
+++ src/componentes/weatherapi.js
@@ -1,64 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import CardApi from './cardapi';
+import Barrabuscar from './buscar.js';
 
 const WeatherApp = () => {
+
   const apiKey = 'f70f771f8fd64f27822214051231305';
   const urlWeather = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=`;
   const urlCity = '&aqi=no';
-
-  const [city, setCity] = useState('');
-  const [cityData, setCityData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [show, setShow] = useState(false);
-
-  const handleCityChange = (event) => {
-    setCity(event.target.value);
-  };
-
-  const handleSearch = () => {
-    getLocation(city);
-  };
-
-  const getLocation = async (loc) => {
-    setLoading(true);
-    const apiUrl = urlWeather + loc + urlCity;
+  
+  const [cityData, setCityData] = useState(null); // almacena datos de la ciudad
+  const [loading, setLoading] = useState(false); //  indica si se esta cargandando la informacion
+  const [show, setShow] = useState(false); // controla la la visivilidad de las tarjetas del clima 
+  
+  const getLocation = async (loc) => { // Función para obtener la ubicación y los datos meteorológicos
+    setLoading(true); //// Se establece el estado de carga a true
+    const apiUrl = urlWeather + loc + urlCity; // Se construye la URL de la API
     try {
-      const response = await fetch(apiUrl);
+      const response = await fetch(apiUrl); // Se realiza la petición a la API
       if (!response.ok) {
         throw new Error('Error en la respuesta de la API');
       }
-      const weatherData = await response.json();
+      const weatherData = await response.json();  // Se obtiene la respuesta en formato JSON
       console.log(weatherData);
-      const cityData = {
+      const cityData = {  // Se extraen los datos relevantes de la respuesta
         name: weatherData.location.name,
         country: weatherData.location.country,
         region: weatherData.location.region,
         temp_c: weatherData.current.temp_c,
         humidity: weatherData.current.humidity,
         condition: weatherData.current.condition.text,
+        precip_mm: weatherData.current.precip_mm,
+        feelslike_c:  weatherData.current.feelslike_c,
         icon: weatherData.current.condition.icon,
         localtime: weatherData.location.localtime,
       };
-      setCityData(cityData);
-      setLoading(false);
-      setShow(true);
+      setCityData(cityData); // Se actualiza el estado con los datos de la ciudad
+      setLoading(false); // Se establece el estado de carga a false
+      setShow(true); // Se muestra la información en la interfaz
     } catch (error) {
       console.log(error);
       setLoading(false);
       setShow(false);
     }
   };
-    const Buscar = 'Buscar'
+  useEffect(() => {
+    if (cityData !== null) {// Cuando cityData cambia y no es null
+    getLocation(''); //Se llama a la función getLocation para obtener los datos de la ubicación inicial
+    }
+  }, []);
+
+    
   return (
     <div>
-      <input type="text" value={city} onChange={handleCityChange} />
-      <button onClick={handleSearch}>Buscar</button>
-
-      {show && <CardApi cityData={cityData} loadingData={loading} showData={show} />}
+       <Barrabuscar newLocation={getLocation}/> 
+      
+      {show && <CardApi cityData={cityData} loadingData={loading} 
+showData={show} />}
     </div>
   );
 };
-
 export default WeatherApp;
 
 
