c34bb3356dcf53ce05934fe4a944fb3ac958b6a4
Karelys Castro <129459178+KarelysCastro21@users.noreply.github.com>
2023-06-09 16:41:17 -0300

    arreglo

--- src/componentes/Testimonios.js
+++ src/componentes/Testimonios.js
@@ -48,7 +48,7 @@ function Acercade() {
                       skill3='Información de radar de precipitaciones.'
                       skill4='Modelo meteorológico EURO1k.'
                       detalle='Combinamos datos meteorológicos actuales e históricos de varias fuentes en todo el mundo, incluidos modelos meteorológicos, datos satelitales, información de radar de precipitaciones, datos oceánicos y observaciones de estaciones. Además, a diferencia de otros proveedores de datos meteorológicos, desarrollamos y fabricamos nuestros propios drones meteorológicos autónomos, Meteodrones, que vuelan hasta 6 km sobre el nivel del suelo para recopilar datos de la capa límite planetaria. Gracias a un permiso especial de la Oficina Federal de Aviación Civil, los meteodrones pueden volar más allá de la línea de visión visual (BVLOS), lo que nos permite recopilar datos incluso dentro de las nubes y la niebla. Integramos los datos recopilados por Meteodrones en el cálculo de nuestro modelo meteorológico EURO1k, brindando pronósticos aún más precisos y confiables. ' />
-                   <Descripcion 
+                   <Testimonio 
                       skill1='Supercomputación y procesamiento de datos.'
                       skill2='Especialistas en TI.'
                         src={require("../img/testimonio-emma.jpg")} 
@@ -56,7 +56,7 @@ function Acercade() {
                       skill4='Pronósticos en tiempo real.'
                       detalle='Sky Alert comprende tanto meteorólogos como especialistas en TI, lo que nos permite ofrecer datos meteorológicos sin precedentes. Hoy, nuestra base de datos contiene más de 15 petabytes de datos meteorológicos y crece cada día. Hacer accesibles cantidades tan grandes de datos está asociado con grandes desafíos. Con nuestro sofisticado sistema de almacenamiento en caché en segundo plano, Meteocache, almacenamos grandes cantidades de observaciones, lo que permite que nuestra API meteorológica vuelva a calcular los pronósticos en tiempo real utilizando la información atmosférica más reciente. Nuestro sistema Meteocache hace coincidir los datos en el tiempo y el espacio, lo que garantiza que incluso los grandes conjuntos de datos se puedan consultar de manera eficiente y rápida en cualquier formato.
                       ' />
-                   <Descripcion 
+                   <Testimonio
                       skill1='Pronósticos hiperlocales y de alta precisión.'
                       skill2='Modelo de terreno digital de la NASA.'
                       src={require("../img/testimonio-sara.jpg")} 
