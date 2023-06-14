66ff3230b27d4e079c8edc8fe1687de701aee871
flornun <florhinupre@gmail.com>
2023-06-13 21:09:46 -0300

    cambios es la ventana

--- src/componentes/modal.js
+++ src/componentes/modal.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+
 import '../hojas-estilos/modal.css'; 
+import React, { useState, useEffect } from 'react';
 
 const Modal = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -11,6 +12,34 @@ const Modal = () => {
   const closeModal = () => {
     setModalVisible(false);
   };
+  useEffect(() => {
+    const handleLinkClick = () => {
+      closeModal(); // Cerrar el modal cuando se hace clic en un enlace del navbar
+    };
+
+    // Agregar un evento de clic a todos los enlaces del navbar
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach((link) => {
+      link.addEventListener('click', handleLinkClick);
+    });
+
+    return () => {
+      // Eliminar los eventos de clic al desmontar el componente
+      navLinks.forEach((link) => {
+        link.removeEventListener('click', handleLinkClick);
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      openModal(); // Abrir el modal después de 3 segundos
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
     <div>
